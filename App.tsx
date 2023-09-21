804af24cc5e0a133c810552cffa11befd55cbe08
José Mateus <jmrc1@aluno.ifal.edu.br>
2023-09-21 18:41:05 -0300

    Adicionando alterações

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button, ScrollView,TextInput,SafeAreaView } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const [altura,setAltura] = React.useState('')
+  const [massa,setMassa] = React.useState('')
+
+  function handleOnchangeAltura(texto: string){
+    setAltura(texto)
+  } 
+
+  function handleOnchangeMassa(texto: string){
+    setMassa(texto)
+  } 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container} >
+      <TextInput style={style.inputs} placeholder='Altura' onChangeText={handleOnchangeAltura} keyboardType='numeric' />
+      <TextInput style={style.inputs} placeholder='Massa' onChangeText={handleOnchangeMassa} keyboardType='numeric' />
+      <Button title='Calcular'/>
+      <Text style={{textAlign:"center",margin:10,}}>{`Altura:${altura}`}</Text>
+      <Text style={{textAlign:"center",margin:10,}}>{`Massa:${massa}`}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = StyleSheet.create({
+    container:{
+      flex:1, // definindo o espaço para toda tela
+      justifyContent:"center", /// todos que estão na horizontal está no centro
+      margin:10, //se descolar 10px para cada lado
+    },
+    inputs:{
+      height: 40, // altura de 40 cm
+      borderColor: 'gray', // borda cinza
+      borderWidth: 1, //borda de altura
+      marginBottom: 10, //se desloca 10px para cima
+      paddingLeft: 10,// cria uma area de 10px na sua esquerda
+    },
+    button:{
+      backgroundColor:"#9ACD32"
+    },
+})
